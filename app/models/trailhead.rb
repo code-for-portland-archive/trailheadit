e1b48169874d3dce4ddf588c72ecad763017b91a
Razaik <razaik@gmail.com>
2014-05-31 23:12:35 -0700

    Adding exifXtractr

--- app/models/trailhead.rb
+++ app/models/trailhead.rb
@@ -1,3 +1,16 @@
 class Trailhead < ActiveRecord::Base
   mount_uploader :photo, TrailheadphotoUploader
+
+  #This method returns the GPS Latitude, Longitude, Timestamp
+  #For a specified image.
+  #Input: Image path for local JPG copy
+  #Output: GPS.latitude, GPS.Longitude, Timestamp
+  #Values may be "Nil" for cases missing Exif info in the JPG
+  
+  def exifXtractr(imgPath)
+    require 'exfir'
+    img= EXIFR::JPEG.new(imgPath)
+    return [img.gps.latitude, img.gps.longitude, img.date_time]
+  end
+  
 end
