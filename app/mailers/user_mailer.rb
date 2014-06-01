1c6ff904264d93698bb19fa0adb233141b99004f
hollocherDale <hollocher@gmail.com>
2014-06-01 12:44:32 -0700

    add mailer & template

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,7 @@
+class UserMailer < ActionMailer::Base
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to My Trailheads')
+  end
+end
