384637a2840d67ad78900090929e767d53110a26
jmoe <me@jmoe.com>
2014-06-03 15:05:31 -0700

    quick commit 2014-06-03 15:05:31

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ActionMailer::Base
-  default from: 'welcome@traileditor.com'
+  default from: 'welcome@traileditor.org'
 
   def welcome_email(user)
     @user = user    
-    mail(from: "welcome@traileditor.com", to: @user.email, subject: 'Welcome to My Trailheads')
+    mail(from: "welcome@traileditor.prg", to: @user.email, subject: 'Welcome to My Trailheads')
   end
 end
