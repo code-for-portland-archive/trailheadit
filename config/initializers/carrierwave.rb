cfe528829f1b81f89e1ae5ba6877b991aa34cd73
jmoe <me@jmoe.com>
2015-01-22 01:10:09 -0800

    quick commit 2015-01-22 01:10:09

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,19 @@
-CarrierWave.configure do |config|  
+CarrierWave.configure do |config|
   config.storage :fog
   config.fog_credentials = {
     :provider               => 'AWS',                         # required
     :aws_access_key_id      =>  ENV['AWS_ACCESS_ID'],         # required
-    :aws_secret_access_key  =>  ENV['AWS_ACCESS_KEY']         # required    
+    :aws_secret_access_key  =>  ENV['AWS_ACCESS_KEY']         # required
   }
   if Rails.env.production?
-    config.fog_directory  = 'trailheadit'
-  elsif Rails.env.staging?    
-    config.fog_directory  = 'trailheaditstaging'
+    config.fog_directory  = 'traileditor-production'
+  elsif Rails.env.staging?
+    config.fog_directory  = 'traileditor-staging'
   else
     config.fog_directory  = 'trailheadit-local'
     config.storage = :file
     config.enable_processing = false
   end
-  config.cache_dir = "#{Rails.root}/tmp/uploads" 
+  config.cache_dir = "#{Rails.root}/tmp/uploads"
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
